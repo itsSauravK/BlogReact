3ec6e13a56be1eaef5db52aeb9e999384d32a219
Saurav <47532084+Saurav@users.noreply.github.com>
2021-06-08 01:09:16 +0530

    Created createPage and added Routes

--- src/Home.js
+++ src/Home.js
@@ -3,12 +3,14 @@ import BlogList from './blogList';
 import useFetch from './useFetch';
 
 
+
 const Home = () => {
 
    const {error, isPending, data: blogs, } = useFetch('http://localhost:8000/blogs');
     
 
     return(
+
         <div className="home">
             {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
