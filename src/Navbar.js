c48bf7e9310fb1cb5d37555b75e4d7599494e0bd
Saurav <47532084+Saurav@users.noreply.github.com>
2021-06-08 16:25:50 +0530

    Added search button

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,43 @@
-import {Link} from 'react-router-dom';
+import { useState } from 'react';
+import {Link, useHistory} from 'react-router-dom';
+import useFetch from './useFetch'
+
+
 
 const Navbar = () =>{
+    
+    const [title, setTitle] = useState('Enter Title');
+    const {data: blogs, error, isPending} = useFetch('http://localhost:8000/blogs');
+    const history = useHistory();
+
+    const handleSearch = (e) =>{
+
+        e.preventDefault();
+        const newBlog = blogs.filter((blog) => blog.title == title)[0];
+        if(newBlog){
+            history.push(`/blogs/${newBlog.id}`)
+        }else{
+            setTitle("Title not found");
+        }
+    }
+    
     
     return(
         <nav className="navbar">
             <h1>Blog</h1>
-            <div className="links">
+            <div className="nav-form">
+                <form onSubmit = {handleSearch}>
+                    <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} />
+                    <button>Search</button>
+                </form>
+                <div className="links">
                 <Link to = "/">Home </Link>
                 <Link to = "/create">New Blog</Link>
             </div>
+            </div>
+            
+          
+            
         </nav>
     );
 
